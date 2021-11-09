8fcb45f4e332607c5e7e5eb1d04fc489f4a82acc
Nguyễn Quang Vinh <hakuno2000@gmail.com>
2021-11-10 06:18:50 +0700

    Fix

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-app>
+    <v-app-bar
+        app
+        color="white"
+    >
+      <div class="d-flex align-center">
+        <v-img
+            alt="Trealet Logo"
+            class="shrink mr-2"
+            contain
+            src="@/assets/logo.png"
+            transition="scale-transition"
+            width="150"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+          href="https://hcloud.trealet.com/apps_dev/btl/nhom03/Streamline/?trealet=/albums/Nhom03/app/hoihoaphuchung.trealet"
+          target="_blank"
+          text
+      >
+        <span class="mr-2">View Trealet</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <div style="display: flex; justify-content: center">
+        <div style="width: 80%">
+          <Info/>
+          <ItemList/>
+        </div>
+      </div>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Info from "@/components/Info";
+import ItemList from "@/components/ItemList";
+
+export default {
+  name: 'App',
+
+  components: {
+    Info,
+    ItemList,
+  },
+
+  data: () => ({
+  }),
+};
+</script>
---
+++ src/components/Info.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-form>
+    <v-container>
+      <v-row>
+        <v-toolbar
+            flat
+        >
+          <v-toolbar-title>Trealet Info</v-toolbar-title>
+          <v-divider
+              class="mx-4"
+              inset
+              vertical
+          ></v-divider>
+        </v-toolbar>
+      </v-row>
+      <v-row>
+        <v-col
+            cols="12"
+            sm="6"
+        >
+          <v-text-field
+              v-model="title"
+              label="Title"
+              outlined
+          ></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col
+            cols="12"
+            sm="6"
+        >
+          <v-text-field
+              v-model="author"
+              label="Author"
+              outlined
+          ></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-container fluid>
+          <v-textarea
+              name="input-7-1"
+              filled
+              label="Description"
+              auto-grow
+              outlined
+              value="Nội dung miêu tả"
+          ></v-textarea>
+        </v-container>
+      </v-row>
+    </v-container>
+  </v-form>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    title: 'Chủ đề của Trealet',
+    author: 'Tác giả',
+  }),
+}
+</script>
+
+<style scoped>
+
+</style>
